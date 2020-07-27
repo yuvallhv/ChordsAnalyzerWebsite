b4a72533134124d05ab30babb8f9357574f5e466
yuvallhv <yuv.lahav@gmail.com>
2020-07-27 18:52:34 +0300

    try remove react msg

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if (process.env.NODE_ENV !== 'development') {
+  console.log = () => {}
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
